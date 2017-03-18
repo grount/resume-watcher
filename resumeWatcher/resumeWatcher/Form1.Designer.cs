5900f008b952fc789b1ee18bef4294de59add26a
Daniel Gront <grount1@gmail.com>
2017-03-18 20:39:23 +0200

    Progress on the company comboBox

--- resumeWatcher/resumeWatcher/Form1.Designer.cs
+++ resumeWatcher/resumeWatcher/Form1.Designer.cs
@@ -28,12 +28,83 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.companyLabel = new System.Windows.Forms.Label();
+            this.companyComboBox = new System.Windows.Forms.ComboBox();
+            this.addButton = new System.Windows.Forms.Button();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.helpToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(740, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.helpToolStripMenuItem.Text = "Help";
+            // 
+            // companyLabel
+            // 
+            this.companyLabel.AutoSize = true;
+            this.companyLabel.Location = new System.Drawing.Point(13, 28);
+            this.companyLabel.Name = "companyLabel";
+            this.companyLabel.Size = new System.Drawing.Size(51, 13);
+            this.companyLabel.TabIndex = 1;
+            this.companyLabel.Text = "Company";
+            // 
+            // companyComboBox
+            // 
+            this.companyComboBox.FormattingEnabled = true;
+            this.companyComboBox.Location = new System.Drawing.Point(13, 45);
+            this.companyComboBox.Name = "companyComboBox";
+            this.companyComboBox.Size = new System.Drawing.Size(121, 21);
+            this.companyComboBox.TabIndex = 2;
+            // 
+            // addButton
+            // 
+            this.addButton.Location = new System.Drawing.Point(214, 408);
+            this.addButton.Name = "addButton";
+            this.addButton.Size = new System.Drawing.Size(75, 23);
+            this.addButton.TabIndex = 3;
+            this.addButton.Text = "Add";
+            this.addButton.UseVisualStyleBackColor = true;
+            this.addButton.Click += new System.EventHandler(this.addButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(740, 458);
+            this.Controls.Add(this.addButton);
+            this.Controls.Add(this.companyComboBox);
+            this.Controls.Add(this.companyLabel);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Resume Watcher";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.Label companyLabel;
+        private System.Windows.Forms.ComboBox companyComboBox;
+        private System.Windows.Forms.Button addButton;
     }
 }
 
--- resumeWatcher/resumeWatcher/Form1.cs
+++ resumeWatcher/resumeWatcher/Form1.cs
@@ -16,5 +16,76 @@ namespace resumeWatcher
         {
             InitializeComponent();
         }
+
+        private enum eErrorType
+        {
+            InvalidInput,
+            Valid,
+            AlreadyExists
+        }
+
+        private void addButton_Click(object sender, EventArgs e)
+        {
+            eErrorType returnedEErrorType = ValidateCompanyComboBoxText();
+            if (returnedEErrorType == eErrorType.Valid)
+            {
+                companyComboBox.Items.Add(companyComboBox.Text);
+            }
+            else
+            {
+                companyComboBox.Text = "";  // If wrong input clear the input.
+
+                if (returnedEErrorType == eErrorType.InvalidInput)
+                {
+                    MessageBox.Show("Please enter a valid input", "Invalid Input", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                else
+                {
+                    MessageBox.Show("Already exists in the company list", "Duplicate Input", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+        }
+
+        private eErrorType ValidateCompanyComboBoxText()
+        {
+            if (companyComboBox.Items.Contains(companyComboBox.Text))
+                // If the written name is there don't add it twice.
+            {
+                return eErrorType.AlreadyExists;
+            }
+
+            if (string.IsNullOrWhiteSpace(companyComboBox.Text))
+            {
+                return eErrorType.InvalidInput;
+            }
+
+            companyComboBox.Text = companyComboBox.Text.Trim();
+            RemoveSpacesBetweenString();
+
+            return eErrorType.Valid;
+        }
+
+        private void RemoveSpacesBetweenString()
+        {
+            StringBuilder newComboBoxText = new StringBuilder();
+            bool isSpaceFound = false;
+
+            for (int i = 0; i < companyComboBox.Text.Length; i++)
+            {
+                char letter = companyComboBox.Text[i];
+
+                if (Char.IsWhiteSpace(letter) == false)
+                {
+                    newComboBoxText.Append(letter);
+                    isSpaceFound = false;
+                }
+                else if (Char.IsWhiteSpace(letter) == true && isSpaceFound == false)
+                {
+                    newComboBoxText.Append(letter);
+                    isSpaceFound = true;
+                }
+            }
+            companyComboBox.Text = newComboBoxText.ToString();
+        }
     }
 }
